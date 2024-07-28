0dce71e50a94dbe755363b92b7d25ff27803be04
Matheus Oliveira <53400866+matheustkful@users.noreply.github.com>
2024-07-28 10:07:19 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -23,7 +23,7 @@ lottieContainer.style.cssText = `
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    background-color: rgba(0, 0, 0, 0.8);
+    background-color: rgba(255, 0, 0, 0.8);
 `;
 lottieContainer.style.display = 'none';
 
