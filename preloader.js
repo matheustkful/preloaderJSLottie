d9c22ba04fe6cb404c25bfa1e569b5de0aaa3630
Matheus Oliveira <53400866+matheustkful@users.noreply.github.com>
2024-07-28 09:58:35 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -38,7 +38,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://github.com/matheustkful/preloaderJSLottie/blob/main/preLoader.json',
+        path: 'https://matheustkful.github.io/preloaderJSLottie/preLoader.json',
     });
 }
 
